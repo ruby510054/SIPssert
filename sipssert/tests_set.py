ruby510054/SIPssert

commit ab5cda90c2f099c5dadcb7fa7a0f98a85ce78268

fail tests on failed init_tasks

sipssert/tests_set.py
@@ -24,6 +24,7 @@ import time
 from sipssert import config
 from sipssert import scenario
 from sipssert import logger
+from sipssert import testing
 from sipssert import tasks_list
 from sipssert import tests_filters
 from sipssert.network import network
@@ -118,6 +119,12 @@ class TestsSet():
             self.init_tasks.run()
         except Exception as e:
             logger.slog.exception(e)
+            self.controller.tlogger.fail()
+            return
+        if self.init_tasks.status != testing.TestStatus.PASS:
+            self.controller.tlogger.fail()
+            logger.slog.warn("initializing tasks failed")
+            logger.slog.debug("tests set executed in {:.3f}s".format(time.time() - start_time))
             return
         try:
             for scen in self.scenarios:

sipssert/testing.py
@@ -123,7 +123,10 @@ class Testing:
         self.emit((f"{self.success_no + self.failed_no}").ljust((self.summary_size - len(summary_name)) // 3, " "), "")
         self.emit((f"{self.success_no}").ljust((self.summary_size - len(summary_name))// 3, " "), "")
         self.emit((f"{self.failed_no}").ljust((self.summary_size - len(summary_name))// 3, " "))
-        return self.failed_no != 0
+        return self.failed_no != 0 or self.force_failed
+
+    def fail(self):
+        self.force_failed = True
 
     def __init__(self, header):
         try:
@@ -140,6 +143,7 @@ class Testing:
         self.test_name_avail = self.tab_size - self.max_status_len
         self.success_no = 0
         self.failed_no = 0
+        self.force_failed = False
         self.emit_header(header)
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4

commit 6eaedba739110430e2bd767f4be5ca3049f8e669

tests_set: proper cleanup of networking if tests fail
Bug introduced in ab5cda9
Many thanks to Maksym Sobolyev (@sobomax) for reporting it
Close #13

sipssert/tests_set.py
@@ -24,7 +24,6 @@ import time
 from sipssert import config
 from sipssert import scenario
 from sipssert import logger
-from sipssert import testing
 from sipssert import tasks_list
 from sipssert import tests_filters
 from sipssert.network import network
@@ -115,23 +114,23 @@ class TestsSet():
     def run(self):
         """Runs one or all tests in a set"""
         start_time = time.time()
+        failure = False
         try:
             self.init_tasks.run()
         except Exception as e:
             logger.slog.exception(e)
             self.controller.tlogger.fail()
-            return
-        if self.init_tasks.status != testing.TestStatus.PASS:
+            failure = True
+        if failure:
             self.controller.tlogger.fail()
             logger.slog.warn("initializing tasks failed")
-            logger.slog.debug("tests set executed in {:.3f}s".format(time.time() - start_time))
-            return
-        try:
-            for scen in self.scenarios:
-                scen.run()
-        except Exception as e:
-            logger.slog.exception(e)
-        self.cleanup_tasks.run(force_all=True)
+        else:
+            try:
+                for scen in self.scenarios:
+                    scen.run()
+            except Exception as e:
+                logger.slog.exception(e)
+            self.cleanup_tasks.run(force_all=True)
         if not self.controller.no_delete:
             # cleanup networks
             for net in self.networks: